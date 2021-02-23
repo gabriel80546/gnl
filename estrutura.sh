05373d92dd906083dd18b33f0bd59499fbdc84f5
gabriel <gabriel80546@gmail.com>
2021-02-23 15:03:26 -0300

    nova estrutura

--- estrutura.sh
+++ estrutura.sh
@@ -1,4 +1,9 @@
 # git clone https://github.com/mrjvs/42cursus_gnl_tests
+caminho=$(pwd)
 mkdir testeum
 git clone https://github.com/mrjvs/42cursus_gnl_tests testeum/repo
 cp -r -v -n testeum/42cursus_gnl_tests/ testeum/copia
+#echo $caminho
+caminho=$(echo $caminho | sed 's/\//\\\//g')
+#echo $caminho
+cat refazer.sh | sed "s/fWU3KdermTW3n9FRbtXwZ7AM/$caminho/g" > temp; mv temp refazer.sh
--- refazer.sh
+++ refazer.sh
@@ -1,1 +1,3 @@
-cp -r -v -n gnl/ gnlcopia
+caminho=fWU3KdermTW3n9FRbtXwZ7AM
+rm -rf -- /home/gabriel/Desktop/qd/projetos/gnl/gnlcopia
+cp -r -v -n /home/gabriel/Desktop/qd/projetos/gnl/gnl/ /home/gabriel/Desktop/qd/projetos/gnl/gnlcopia

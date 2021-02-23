c8b99836679f567bcb2bc6e79563d6138c8736d4
gabriel <gabriel80546@gmail.com>
2021-02-23 15:06:52 -0300

    estrutura quase pronta

--- estrutura.sh
+++ estrutura.sh
@@ -2,8 +2,11 @@
 caminho=$(pwd)
 mkdir testeum
 git clone https://github.com/mrjvs/42cursus_gnl_tests testeum/repo
-cp -r -v -n testeum/42cursus_gnl_tests/ testeum/copia
+cp -r -v -n testeum/repo testeum/copia
 #echo $caminho
 caminho=$(echo $caminho | sed 's/\//\\\//g')
 #echo $caminho
-cat refazer.sh | sed "s/fWU3KdermTW3n9FRbtXwZ7AM/$caminho/g" > temp; mv temp refazer.sh
+cp gnl/refazer.sh .
+cp gnl/cmd .
+cat refazer.sh | sed "s/fWU3KdermTW3n9FRbtXwZ7AM/$caminho/g" > temp
+mv temp refazer.sh

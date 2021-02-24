ac36f37a74ef14308999468f1d76ccbfb491dcab
gabriel <gabriel80546@gmail.com>
2021-02-24 12:58:22 -0300

    algo nao esta certo

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 14:06:33 by gpassos-          #+#    #+#             */
-/*   Updated: 2021/02/24 12:09:27 by gabriel          ###   ########.fr       */
+/*   Updated: 2021/02/24 12:58:05 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -130,9 +130,9 @@ int		get_next_line(int fd, char **line)
 	static char	*buffer;
 	static int	line_number = 0;
 	static int	last_offset = 0;
-	char		*temp_line;
-	int			read_saida;
-	int			i;
+	// char		*temp_line;
+	// int			read_saida;
+	// int			i;
 	// int			j;
 
 	int			debug;
@@ -148,77 +148,5 @@ int		get_next_line(int fd, char **line)
 	if(debug == 1) { printf("148: last_offset = %d\n", last_offset); }
 	if(debug == 1) { printf("149: --------------------\n"); }
 
-	if(BUFFER_SIZE <= 0 || fd < 0 || line == NULL || fd > RLIMIT_NOFILE)
-		return (-1);
-
-	if (line_number == 0)
-	{
-		if(debug == 1) { printf("156: primeira linha entao vou inicializar o buffer\n"); }
-		buffer = (char *)ft_calloc(sizeof(char), BUFFER_SIZE + 1);
-		if (buffer == NULL)
-			return (-1);
-	}
-
-	if(debug == 1) { printf("162: inicializando temp_line\n"); }
-	temp_line = (char *)ft_calloc(sizeof(char), BUFFER_SIZE + 1);
-	if (temp_line == NULL)
-		return (-1);
-
-
-	i = 0;
-	while (1)
-	{
-		if(debug == 1) { printf("171: temp_line = '"); }
-		if(debug == 1) { write(1, temp_line, BUFFER_SIZE); }
-		if(debug == 1) { printf("'\n"); }
-		if(debug == 1) { printf("174: lendo arquivo\n"); }
-		read_saida = read(fd, buffer, BUFFER_SIZE);
-		buffer[BUFFER_SIZE] = '\0';
-		if(debug == 1) { printf("177: foi lido %d\n", read_saida); }
-
-		if (read_saida == 0)
-		{
-			if(debug == 1) { printf("181: nada foi lido EOF atingido\n"); }
-			*line = temp_line;
-			line_number++;
-			return (0);
-		}
-		else if (read_saida <= BUFFER_SIZE)
-		{
-			if(debug == 1) { printf("188: tem coisa para passar pro temp_line\n"); }
-			if(debug == 1) { printf("189: tem '\\n' nesse buffer? %s\n", (ft_memchr(buffer, '\n', BUFFER_SIZE) != NULL) ? "sim" : "nao"); }
-			if (ft_memchr(buffer, '\n', BUFFER_SIZE) != NULL)
-			{
-				if(debug == 1) { printf("192: tem '\\n' nesse buffer\n"); }
-				if(debug == 1) { printf("193: \"caso tenha 1 ou mais '\\n' dentro do buffer nao faça mais chamadas para read\"\n"); }
-				if(debug == 1) { printf("194: buffer = '%s'(%ld)\n", buffer, ft_strlen(buffer)); }
-				if(debug == 1) { printf("195: esse '\\n' está na posicao %ld\n", (char *)ft_memchr(buffer, '\n', BUFFER_SIZE) - buffer); }
-				if(debug == 1) { printf("196: então vou copiar até ai no temp_line\n"); }
-
-				ft_memcpy(temp_line, buffer, ((char *)ft_memchr(buffer, '\n', BUFFER_SIZE) - buffer));
-				last_offset += ((char *)ft_memchr(buffer, '\n', BUFFER_SIZE) - buffer);
-				if(debug == 1) { printf("200: last_offset = %d\n", last_offset); }
-				line_number++;
-				if(debug == 1) { printf("202: copiando temp_line para *line e liberando a memoria\n"); }
-				*line = (char *)malloc(sizeof(char) * last_offset);
-				if (*line == NULL)
-					return (-1);
-				ft_memcpy(*line, temp_line, last_offset);
-				free(temp_line);
-				return (1);
-			}
-			else
-			{
-				if(debug == 1) { printf("212: não tem '\\n' nesse buffer\n"); }
-				if(debug == 1) { printf("213: todo conteudo do buffer é copiado no temp_line. e continua\n"); }
-				if(debug == 1) { printf("214: buffer = '%s'(%ld)\n", buffer, ft_strlen(buffer)); }
-			}
-		}
-		else
-			return (-1);
-		if (i >= 2)
-			break ;
-		i++;
-	}
 	return (-1);
 }

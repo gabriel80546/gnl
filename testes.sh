b0dd1a9005ba324e7175b3f1296966399a496291
Gabriel <gabriel80546@gmail.com>
2021-03-01 09:55:01 -0300

    novo script

---
+++ testes.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/Tripouille/gnlTester
+git clone https://github.com/Hellio404/Get_Next_Line_Tester
+git clone https://github.com/Mazoise/42TESTERS-GNL
+git clone https://github.com/charMstr/GNL_lover
+git clone https://github.com/lgrellie/gnl_tester
